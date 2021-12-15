ddab0474cc9cf1997cedc489ec4511a38610e9d6
MahmoudRedaSayed <76118788+MahmoudRedaSayed@users.noreply.github.com>
2021-12-15 09:01:42 +0200

    some things in access needed to be  fixed but after the login page

--- Logout.py
+++ Logout.py
@@ -1,8 +1,12 @@
 from os import abort
-from flask import render_template,Blueprint,request, session
+from flask import render_template,Blueprint,request, session,Response,make_response
 from werkzeug.utils import redirect
 LogoutPage=Blueprint("Logout",__name__)
 @LogoutPage.route("/logout")
 def logout():
+    resp = make_response( redirect("/home"))
+    resp.set_cookie('username', expires=0)
     session.clear()
-    return redirect("/home")
+    return resp
+    
+    # return redirect("/home")
